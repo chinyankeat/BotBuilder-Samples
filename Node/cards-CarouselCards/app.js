840d0d779c36d292aa4f782f40355acf12120113
Chin Yan Keat <chinyankeat@yahoo.com>
2017-04-05 19:27:38 +0800

    Update app.js

--- Node/cards-CarouselCards/app.js
+++ Node/cards-CarouselCards/app.js
@@ -21,12 +21,31 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     var cards = getCardsAttachments();
 
-    // create reply with Carousel AttachmentLayout
-    var reply = new builder.Message(session)
-        .attachmentLayout(builder.AttachmentLayout.carousel)
-        .attachments(cards);
+    
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                     " you've been programming for " + session.userData.coding + 
+                     " years and use " + session.userData.language + ".");
+    
+        // create reply with Carousel AttachmentLayout
+        var reply = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments(cards);
 
-    session.send(reply);
+        session.send(reply);
+    }
 });
 
 function getCardsAttachments(session) {
